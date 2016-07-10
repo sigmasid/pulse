9806bdf7786042d500b1c7da9b16c8de52b6c84d
sid_tiwari <sidtiwari@sidharths-macbook-pro.local>
2016-07-10 00:37:52 -0700

    email password checks

--- Pulse/LoginCreateAccountVC.swift
+++ Pulse/LoginCreateAccountVC.swift
@@ -8,31 +8,37 @@
 
 import UIKit
 
-class LoginCreateAccountVC: UIViewController {
+class LoginCreateAccountVC: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var logoView: UIView!
     @IBOutlet weak var userPassword: UITextField!
     @IBOutlet weak var userEmail: UITextField!
-    
-    let _errorLabel = UILabel()
+    @IBOutlet weak var _emailErrorLabel: UILabel!
+    @IBOutlet weak var _passwordErrorLabel: UILabel!
+    @IBOutlet weak var signupButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
-
+        
+        self.userEmail.delegate = self
+        self.userPassword.delegate = self
     }
     
     override func viewDidAppear(animated : Bool) {
         super.viewDidAppear(true)
         
         let pulseIcon = Icon(frame: CGRectMake(0,0,self.logoView.frame.width, self.logoView.frame.height))
-        pulseIcon.drawIcon(iconColor, iconThickness: 3)
         pulseIcon.drawIconBackground(iconBackgroundColor)
+        pulseIcon.drawIcon(iconColor, iconThickness: 3)
 
         logoView.addSubview(pulseIcon)
         
         self.userEmail.layer.addSublayer(addBorders(self.userEmail))
         self.userPassword.layer.addSublayer(addBorders(self.userPassword))
+        
+        self.signupButton.layer.cornerRadius = 20
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,33 +50,27 @@ class LoginCreateAccountVC: UIViewController {
     @IBAction func createEmailAccount(sender: UIButton) {
         validateEmail(userEmail.text, completion: {(verified, error) in
             if !verified {
-                print(error!.localizedDescription)
+                self._emailErrorLabel.text = error!.localizedDescription
             } else {
                 self.validatePassword(self.userPassword.text, completion: {(verified, error) in
                     if !verified {
-                        print(error!.localizedDescription)
+                        self._passwordErrorLabel.text = error!.localizedDescription
                     } else {
-                        print("valid email and password")
+                        self.performSegueWithIdentifier("addNameSegue", sender: self)
                     }
                 })
             }
         })
-        
-
     }
     
     ///Validate email
     private func validateEmail(enteredEmail:String?, completion: (verified: Bool, error: NSError?) -> Void) {
-        completion(verified: true, error: nil)
-        
         let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
         
         if emailPredicate.evaluateWithObject(enteredEmail) {
-            print("valid email fired")
             completion(verified: true, error: nil)
         } else {
-            print("invalid email fired")
             let userInfo = [ NSLocalizedDescriptionKey : "Please enter a valid email" ]
             completion(verified: false, error: NSError.init(domain: "Invalid", code: 0, userInfo: userInfo))
         }
@@ -78,32 +78,24 @@ class LoginCreateAccountVC: UIViewController {
     
     ///Validate password returns true if validated, error otherwise
     private func validatePassword(enteredPassword:String?, completion: (verified: Bool, error: NSError?) -> Void) {
-        completion(verified: true, error: nil)
-        
         let passwordFormat = "^(?=.*?[a-z]).{8,}$"
         let passwordPredicate = NSPredicate(format:"SELF MATCHES %@", passwordFormat)
         
         if passwordPredicate.evaluateWithObject(enteredPassword) {
-            print("valid email fired")
             completion(verified: true, error: nil)
+            return
         } else {
-            print("invalid password fired")
-            let userInfo = [ NSLocalizedDescriptionKey : "Password must be 8 characters in length." ]
+            let userInfo = [ NSLocalizedDescriptionKey : "Password must be 8 characters in length" ]
             completion(verified: false, error: NSError.init(domain: "Invalid", code: 0, userInfo: userInfo))
+            return
         }
 //        let passwordFormat = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
 //        let passwordPredicate = NSPredicate(format:"SELF MATCHES %@", passwordFormat)
     }
     
-    private func setupErrorLabel(_relatedLabel : UILabel, error : String) {
-        _errorLabel.text = error
-        _errorLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleCaption1)
-        _errorLabel.textColor = UIColor.redColor()
-        _errorLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        _errorLabel.topAnchor.constraintEqualToAnchor(_relatedLabel.bottomAnchor, constant: 0).active = true
-        _errorLabel.widthAnchor.constraintEqualToAnchor(_relatedLabel.widthAnchor).active = true
-        _errorLabel.centerXAnchor.constraintEqualToAnchor(_relatedLabel.centerXAnchor, constant: 0).active = true
+    func textFieldDidBeginEditing(textField: UITextField) {
+        _passwordErrorLabel.text = ""
+        _emailErrorLabel.text = ""
     }
     
     /*
