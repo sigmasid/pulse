fcaca9262a666dd4133ad2c65fe4defd8039cdc6
sid_tiwari <sidtiwari@sidharths-macbook-pro.local>
2016-08-26 13:12:52 -0700

    first commit for revised explore page

---
+++ Pulse/ExploreCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreCell.swift
+//  Pulse
+//
+//  Created by Sidharth Tiwari on 8/26/16.
+//  Copyright © 2016 Think Apart. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+    
+}
---
+++ Pulse/ExploreVC.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreVC.swift
+//  Pulse
+//
+//  Created by Sidharth Tiwari on 8/26/16.
+//  Copyright © 2016 Think Apart. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
